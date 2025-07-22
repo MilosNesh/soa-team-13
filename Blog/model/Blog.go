8682a3a79a3dc960e2c26f3a3e22de23108e22cf
nikolapaca <nikolapaunovic15@gmail.com>
2025-07-22 16:50:49 +0200

    Creating and getting blogs from MongoDB

---
+++ Blog/model/Blog.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Blog struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Title     string             `bson:"title" json:"title"`
+	Content   string             `bson:"content" json:"content"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	ImageUrl  string             `bson:"image_url,omitempty" json:"image_url,omitempty"`
+}
